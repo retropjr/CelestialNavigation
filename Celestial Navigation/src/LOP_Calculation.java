b559b2aba3ccc1fbbb3a3486f500b46140182263
retropjr@hotmail.com <retropjr@hotmail.com>
2021-02-06 04:00:51 +0000

    set up, added sun object

--- Celestial Navigation/src/LOP_Calculation.java
+++ Celestial Navigation/src/LOP_Calculation.java
@@ -3,7 +3,7 @@ public class LOP_Calculation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new SunCalculation();
+		SunCalculation.plot();
 	}
 
 }
--- Celestial Navigation/src/SunCalculation.java
+++ Celestial Navigation/src/SunCalculation.java
@@ -1,5 +1,11 @@
 
 public class SunCalculation {
 
-
+	
+	
+	public static void plot() {
+		System.out.println("Plot : " + UserInputs.WINDOW_HEIGHT);
+	
+	}
 }
+ 
